c643df8d8dc23b0a1880ab040db6158da46a807a
Arno Galvez <arno.galvez@gmail.com>
2019-11-16 15:33:09 +0100

    Take enum form cblas.h

--- lib/my_lapack.h
+++ lib/my_lapack.h
@@ -1,19 +1,15 @@
 #ifndef MY_LAPACK_H
 #define MY_LAPACK_H
 
-namespace my_lapack {
+#include "cblas.h"
 
-    enum CBLAS_LAYOUT { CblasRowMajor = 101, CblasColMajor = 102 };
-    enum CBLAS_TRANSPOSE { CblasNoTrans = 111, CblasTrans = 112, CblasConjTrans = 113 };
-    enum CBLAS_UPLO { CblasUpper = 121, CblasLower = 122 };
-    enum CBLAS_DIAG { CblasNonUnit = 131, CblasUnit = 132 };
-    enum CBLAS_SIDE { CblasLeft = 141, CblasRight = 142 };
+namespace my_lapack {
 
     double my_ddot( const int N, const double *X, const int incX, const double *Y, const int incY );
 
     void my_daxpy( const int N, const double alpha, const double *X, const int incX, double *Y, const int incY );
 
-    void my_dgemv( enum CBLAS_LAYOUT    layout,
+    void my_dgemv( enum CBLAS_ORDER     layout,
                    enum CBLAS_TRANSPOSE TransA,
                    int                  M,
                    int                  N,
@@ -26,7 +22,7 @@ namespace my_lapack {
                    double *             Y,
                    const int            incY );
 
-    void my_dgemm_scalaire( enum CBLAS_LAYOUT    Order,
+    void my_dgemm_scalaire( enum CBLAS_ORDER     layout,
                             enum CBLAS_TRANSPOSE TransA,
                             enum CBLAS_TRANSPOSE TransB,
                             int                  M,
@@ -66,6 +62,26 @@ namespace my_lapack {
                    double               beta,
                    double *             C,
                    int                  ldc );
+
+    //************************************
+    // Method:    my_dgetrf2
+    // FullName:  my_lapack::my_dgetrf2
+    // Access:    public
+    // Returns:   void
+    // Parameter: int M - Number of rows of matrix A
+    // Parameter: int N - Number of columns of matrix A
+    // Parameter: double * A - Dim (lda, N). Upon exit, holds L, U such that A = P * L * U
+    // Parameter: int lda - Leading dimension of matrix A
+    // Parameter: int * ipiv - Dim (min(M,N)). Pivot indices, row i of A was interchanged with ipiv[i]
+    // Parameter: int * info - 0 if success, if info > 0 then U(info - 1,info - 1) == 0.0
+    //************************************
+    void my_dgetrf2( int M, int N, double *A, int lda, int *ipiv, int *info );
+
+    int my_idamax( int N, double *dx, int incX );
+
+    void my_dscal( int N, double da, double *dx, int incX );
+
+    void my_dlaswp( int N, double *A, int lda, int k1, int k2, int *ipv, int incX );
 
 } // namespace my_lapack
 #endif
