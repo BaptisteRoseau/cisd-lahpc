6e36928017c53238ca8d8bf902e4248082e7c9e9
Arno Galvez <arno.galvez@gmail.com>
2019-12-11 09:25:05 +0100

    Test with call to my_pdgemm

--- perf/benchmark.cpp
+++ perf/benchmark.cpp
@@ -76,7 +76,7 @@ void benchmark_summa( int argc, char **argv )
     SUMMA.init( &argc, &argv );
     SUMMA.reset( M, N, K );
 
-    /*if ( SUMMA.rankWorld() == 0 ) {
+    if ( SUMMA.rankWorld() == 0 ) {
         Mat A( M, K, 1. ), B( K, N, 1. ), C( M, N, 1. );
         my_dgemm_mpi( CblasColMajor,
                       CblasNoTrans,
@@ -92,7 +92,9 @@ void benchmark_summa( int argc, char **argv )
                       0.,
                       C.get(),
                       C.ld() ); 
-    }*/
+    }
+
+    SUMMA.finalize();
 }
 
 int main( int argc, char **argv )
