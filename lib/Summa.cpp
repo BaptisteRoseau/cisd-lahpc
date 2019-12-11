87c15691247690db60077f83b85be34e0d3dd06e
Arno Galvez <arno.galvez@gmail.com>
2019-12-11 18:46:12 +0100

    Does it run this way ?

--- lib/Summa.cpp
+++ lib/Summa.cpp
@@ -224,7 +224,10 @@ void Summa::sendBlockWorld( int emitter, int receiver, int M, int N, const doubl
     else if ( rankWorld_ == receiver ) {
         MPI_Status status;
         // std::cout << "Receiving block..." << std::endl;
-        MPI_Recv( b, M * N, MPI_DOUBLE, emitter, 0, MPI_COMM_WORLD, &status );
+        double *recvBuff = new double[M * N];
+        MPI_Recv( recvBuff, M * N, MPI_DOUBLE, emitter, 0, MPI_COMM_WORLD, &status );
+        my_lapack::my_dlacpy( M, N, recvBuff, M, b, ldb );
+        delete[] recvBuff;
         // std::cout << "Receive done." << std::endl;
     }
 }
