35943ab54537c55dfa2e6e2d3ee375b6f4a885b3
Arno Galvez <arno.galvez@gmail.com>
2019-12-12 10:39:48 +0100

    Fix install script

--- install.sh
+++ install.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/bash
 
-if [ $# -le 1 ]
+if [ $# -le 1 ]; then
     echo "[INSTALL] No argument provided, default is Debug"
-elif [ ! $1 = "Debug" -a ! $1 = "Release" ]
-    echo "[INSTALL] Incorrect configuration. Possible values are Debug and Release"
-else
-    rm -rf build
-    mkdir build
-    cd build
-    cmake3 -DCMAKE_BUILD_TYPE=$1 
+elif [ ! $1 = "Debug" -a ! $1 = "Release" ]; then
+    echo "[INSTALL] Incorrect configuration. Possible values are Debug and Release";
+    exit 0
 fi
+
+rm -rf build
+mkdir build
+cd build
+cmake3 -DCMAKE_BUILD_TYPE=$1 
