ea383cef93f5dab06c7b4dece5d98ac5200c546c
Arno Galvez <arno.galvez@gmail.com>
2019-12-12 11:13:33 +0100

    Update install scripts

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,15 +1,20 @@
 
-if [ $# -le 1 ]; then
-    echo "[INSTALL] No argument provided, default is Debug."
+buildType=Debug
+
+if [ $# -le 0 ]; then
+    echo "\n[INSTALL] No argument provided, default is Debug.\n"
 elif [ ! $1 = "Debug" -a ! $1 = "Release" ]; then
-    echo "[INSTALL] Incorrect configuration. Possible values are Debug and Release.";
+    echo "\n[INSTALL] Incorrect configuration. Possible values are Debug and Release.\n";
     exit 0
+else
+    buildType=$1;
+    echo "\n[INSTALL] Build type is $buildType\n"
 fi
 
 currDir=$(pwd)
 
 mkdir -p build && cd build && \
-cmake -DCMAKE_BUILD_TYPE=$1 .. && make -j8
+cmake -DCMAKE_BUILD_TYPE=$buildType -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON .. && make -j8
 
 if ! [ $? -eq 0 ]; then
     echo "Please run this script from the project root directory using 'source'."
--- scripts/install_plafrim.sh
+++ scripts/install_plafrim.sh
@@ -3,13 +3,13 @@
 buildType=Debug
 
 if [ $# -le 0 ]; then
-    echo "[INSTALL] No argument provided, default is Debug."
+    echo "\n[INSTALL] No argument provided, default is Debug.\n"
 elif [ ! $1 = "Debug" -a ! $1 = "Release" ]; then
-    echo "[INSTALL] Incorrect configuration. Possible values are Debug and Release.";
+    echo "\n[INSTALL] Incorrect configuration. Possible values are Debug and Release.\n";
     exit 0
 else
     buildType=$1;
-    echo "[INSTALL] Build type is $buildType"
+    echo "\n[INSTALL] Build type is $buildType\n"
 fi
 
 currDir=$(pwd)
@@ -17,7 +17,7 @@ currDir=$(pwd)
 module load compiler/gcc/8.3.0 linalg/mkl/2019_update4 compiler/cuda/10.1 mpi/openmpi/4.0.1
 
 mkdir -p build && cd build && \
-cmake3 -DCMAKE_BUILD_TYPE=$buildType .. && make -j20 
+cmake3 -DCMAKE_BUILD_TYPE=$buildType -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON .. && make -j20 
 
 if ! [ $? -eq 0 ]; then
     echo "Please run this script from the project root directory using 'source'."
