b080f0e9ee8e342478d7d5e3ec3bab92309323f8
Arno Galvez <arno.galvez@gmail.com>
2019-12-12 11:07:54 +0100

    Fix install plafrim

--- scripts/install_plafrim.sh
+++ scripts/install_plafrim.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/bash
 
-if [ $# -le 1 ]; then
+buildType=Debug
+
+if [ $# -le 0 ]; then
     echo "[INSTALL] No argument provided, default is Debug."
 elif [ ! $1 = "Debug" -a ! $1 = "Release" ]; then
     echo "[INSTALL] Incorrect configuration. Possible values are Debug and Release.";
     exit 0
+else
+    buildType=$1;
+    echo "[INSTALL] Build type is $buildType"
 fi
 
 currDir=$(pwd)
@@ -12,7 +17,7 @@ currDir=$(pwd)
 module load compiler/gcc/8.3.0 linalg/mkl/2019_update4 compiler/cuda/10.1 mpi/openmpi/4.0.1
 
 mkdir -p build && cd build && \
- cmake3 -DCMAKE_BUILD_TYPE=$1 .. && make -j20 
+cmake3 -DCMAKE_BUILD_TYPE=$buildType .. && make -j20 
 
 if ! [ $? -eq 0 ]; then
     echo "Please run this script from the project root directory using 'source'."
