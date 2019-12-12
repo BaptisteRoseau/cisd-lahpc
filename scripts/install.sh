b9ef85947871dcea2869e2f678a6cba2b0b80a2f
Arno Galvez <arno.galvez@gmail.com>
2019-12-12 10:51:25 +0100

    Fix install scripts

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,7 +1,15 @@
+
+if [ $# -le 1 ]; then
+    echo "[INSTALL] No argument provided, default is Debug."
+elif [ ! $1 = "Debug" -a ! $1 = "Release" ]; then
+    echo "[INSTALL] Incorrect configuration. Possible values are Debug and Release.";
+    exit 0
+fi
+
 currDir=$(pwd)
 
 mkdir -p build && cd build && \
-cmake .. && make -j8
+cmake -DCMAKE_BUILD_TYPE=$1 .. && make -j8
 
 if ! [ $? -eq 0 ]; then
     echo "Please run this script from the project root directory."
--- scripts/install_plafrim.sh
+++ scripts/install_plafrim.sh
@@ -3,7 +3,7 @@ currDir=$(pwd)
 module load compiler/gcc/8.3.0 linalg/mkl/2019_update4 compiler/cuda/10.1 mpi/openmpi/4.0.1
 
 mkdir -p build && cd build && \
- cmake3 .. && make -j20 
+ cmake3 -DCMAKE_BUILD_TYPE=$1 .. && make -j20 
 
 if ! [ $? -eq 0 ]; then
     echo "Please run this script from the project root directory."
